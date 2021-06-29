deb802895da93b5ea30e150cb69646745961b4c2
Giovannimb87 <Giovanni.brex@hotmail.com>
2021-06-29 03:07:06 +0000

    Submit

--- rover.js
+++ rover.js
@@ -1,5 +1,63 @@
 class Rover {
-   // Write code here!
-}
+    constructor(position, mode = 'NORMAL', generatorWatts = 110) {
+        this.mode = mode;
+        this.generatorWatts = generatorWatts;
+        this.position = position;
+    }
+
+    receiveMessage(message) {
+      let response = {
+        message : message.name,
+        results : []
+      };
+
+
+      for (const command of message.commands) {
+
+        let commandExecution = {};
+
+            if(command.commandType === 'STATUS_CHECK') {
+            
+              const roverStatus = {};
+
+                roverStatus.mode = this.mode;
+                roverStatus.generatorWatts = this.generatorWatts;
+                roverStatus.position = this.position;
+                commandExecution.completed = true;
+                commandExecution.roverStatus = roverStatus
+
+          } else if(command.commandType === 'MODE_CHANGE') {
+
+              if (command.value === 'LOW_POWER' || command.value === 'NORMAL') {
+
+                this.mode = command.value;
+                commandExecution.completed = true;        
+              
+              } else { commandExecution.completed = false;
+                  
+              }
+
+          } else if(command.commandType === 'MOVE') {
+
+              if(this.mode === 'NORMAL'){
+
+                this.position = command.value;
+                commandExecution.completed = true;
+              }else {
+            commandExecution.completed = false;
+          }
+
+          } else {
+            commandExecution.completed = false;
+          }
+
+          response.results.push(commandExecution);
+      }
+
+      return response;
+
+    };
+
+};
 
 module.exports = Rover;
